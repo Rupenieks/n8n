c65c7fe119d9b9eb70868274cc0c65cfdf78c9be
Rupenieks <ronaldsupenieks96@gmail.com>
2020-04-21 19:07:38 +0200

    Update Db.ts

--- packages/cli/src/Db.ts
+++ packages/cli/src/Db.ts
@@ -21,6 +21,8 @@ import {
 	MySQLDb,
 } from './databases';
 
+
+
 export let collections: IDatabaseCollections = {
 	Credentials: null,
 	Execution: null,
@@ -45,6 +47,8 @@ export async function init(synchronize?: boolean): Promise<IDatabaseCollections>
 				entityPrefix: await GenericHelpers.getConfigValue('database.tablePrefix') as string,
 				url: await GenericHelpers.getConfigValue('database.mongodb.connectionUrl') as string,
 				useNewUrlParser: true,
+				migrationsRun: true,
+				migrations: ["./databases/mongodb/Migrations/*.ts"]
 			};
 			break;
 
@@ -60,6 +64,8 @@ export async function init(synchronize?: boolean): Promise<IDatabaseCollections>
 				port: await GenericHelpers.getConfigValue('database.postgresdb.port') as number,
 				username: await GenericHelpers.getConfigValue('database.postgresdb.user') as string,
 				schema: await GenericHelpers.getConfigValue('database.postgresdb.schema') as string,
+				migrationsRun: true,
+				migrations: ["./databases/postgresdb/Migrations/*.ts"]
 			};
 			break;
 
@@ -75,6 +81,8 @@ export async function init(synchronize?: boolean): Promise<IDatabaseCollections>
 				password: await GenericHelpers.getConfigValue('database.mysqldb.password') as string,
 				port: await GenericHelpers.getConfigValue('database.mysqldb.port') as number,
 				username: await GenericHelpers.getConfigValue('database.mysqldb.user') as string,
+				migrationsRun: true,
+				migrations: ["./databases/mysqldb/Migrations/*.ts"]
 			};
 			break;
 
@@ -85,6 +93,8 @@ export async function init(synchronize?: boolean): Promise<IDatabaseCollections>
 				type: 'sqlite',
 				database: path.join(n8nFolder, 'database.sqlite'),
 				entityPrefix: await GenericHelpers.getConfigValue('database.tablePrefix') as string,
+				migrationsRun: true,
+				migrations: ["./databases/sqlite/Migrations/*.ts"]
 			};
 			break;
 
